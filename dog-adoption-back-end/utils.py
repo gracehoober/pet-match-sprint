9ab41d8bd8e92f1763cb927d77e4883bd6420816
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-01-30 11:45:14 -0800

    created utils.py and generate OAuth token

---
+++ dog-adoption-back-end/utils.py
@@ -0,0 +1,18 @@
+import os
+import requests
+
+PETFINDER_API_KEY = os.environ["petfinder_API_key"]
+PETFINDER_SECRET_KET = os.environ["petfinder_secret_key"]
+PETFINDER_API_URL = "https://api.petfinder.com/v2/oauth2/token"
+
+
+def update_auth_token():
+    """Gets an OAuth token from Petfinder"""
+    
+    data = {"grant_type":"client_credentials"}
+    token = requests.post(
+        PETFINDER_API_URL,
+        data=data,
+        auth=(PETFINDER_API_KEY, PETFINDER_SECRET_KET)
+        )
+    return token.json()
