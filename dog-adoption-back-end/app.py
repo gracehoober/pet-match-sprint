aec9216499f9c281a1fce07f229a52f36e857230
Grace Bethany Hoober <grace.hoober66@gmail.com>
2024-01-30 11:43:53 -0800

    function outline for login and API requests

--- dog-adoption-back-end/app.py
+++ dog-adoption-back-end/app.py
@@ -1,11 +1,31 @@
 from flask import Flask, request
-import os
-# import requests
+from utils import update_auth_token
+import requests
 
-PETFINDER_ACCESS_TOKEN = os.environ["petfinder_access_token"]
+PETFINDER_AUTH_TOKEN = None
 
 app = Flask(__name__)
 
+########## ROUTES ###########
+
+
+@app.before_first_request
+def update_credentials():
+    """Get auth token from Petfinder and store globally"""
+    global PETFINDER_AUTH_TOKEN
+    PETFINDER_AUTH_TOKEN = update_auth_token()
+
+# @app.route("/login", methods=["POST", "GET"])
+# def login():
+#     #get email from the form and see if it is in the db
+#         #if in db then password form should show up
+#         #if not in db redirect to a signup page
+
+
+# @app.post("/signup")
+# def signup():
+#     #get signup info from the form and send it to the db
+#     #login the person
 
 @app.get("/")
 def getDogs():
@@ -18,19 +38,21 @@ def getDogs():
         """
 
 
+# TODO: add in error handling (search term couldnt find something)
 @app.get("/search")
 def searchDogs():
     """Handles dog breed search requests
     Example: "/search?breed=germanshepard"
     """
+    # getting the search term from the request
     breed = request.args["breed"]
 
+    # requesting a list of dogs from the apr
     dogs_by_breed = requests.get("https://api.petfinder.com/v2/dogs",
                                  params={"breed": breed},
                                  headers={"Authorization":
                                           f"Bearer {PETFINDER_ACCESS_TOKEN}"},
                                  )
+    dogs_by_breed = re
+    # sending list of dogs back to the front end -> jsonify()??
     return dogs_by_breed
-
-
-
